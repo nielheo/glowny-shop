577f729415fe8ed2f8e0dac8c30e61eee7e509b8
niel <niel.heo@gmail.com>
2017-05-14 20:40:23 +0700

    remove old admin site

--- thrive-people-admin-old/src/reducers/index.js
+++
@@ -1,9 +0,0 @@
-import { combineReducers } from 'redux'
-import homeReducer from './homeReducer'
-import queryReducer from './queryReducer'
-
-
-export default combineReducers({
-  homeReducer,
-  queryReducer,
-});

f6f73fd33bf09cb395c9582947337dbf16a60ccc
niel <niel.heo@gmail.com>
2017-06-17 22:44:01 +0700

    Unit Test (GraphQL) : Select Products

---
+++ glowny-shop-graphql/src/graphql/tests/products_query.test.js
@@ -0,0 +1,53 @@
+import {tester} from 'graphql-tester'
+import expect from 'expect.js'
+
+describe('viewer/products', () => {
+  let test = tester({
+      url: 'http://localhost:4000/graphql',
+      contentType: 'application/json'
+  })
+
+  describe('select', function () {
+    it('select products count is 2', function () {
+      return test('{ "query": "{ viewer { products(shopCode:\\"glowny_shop\\") { id } } }" }')
+      .then(result => {
+        expect(result.status).to.equal(200)
+        expect(result.success).to.equal(true)
+        expect(result.data.viewer.roles.length).to.equal(2)
+      })
+    })
+
+    it('select products count is 1', function () {
+      return test('{ "query": "{ viewer { products(shopCode:\\"daniel_shop\\") { id } } }" }')
+      .then(result => {
+        expect(result.status).to.equal(200)
+        expect(result.success).to.equal(true)
+        expect(result.data.viewer.roles.length).to.equal(2)
+      })
+    })
+
+    it('select roles without shopCode arg, should return error', function () {
+      return test('{ "query": "{ viewer { products { id } } }" }')
+      .then(result => {
+        expect(result.status).to.equal(400)
+        expect(result.success).to.equal(false)
+      })
+    })
+
+    it('select users first:1 - return all expected fields', function () {
+      return test('{ "query": "{ viewer { products(shopCode:\\"glowny_shop\\") { id sku name description curr price } } }" }')
+      .then(result => {
+        expect(result.status).to.equal(200)
+        expect(result.success).to.equal(true)
+        expect(result.data.viewer.products).to.be.an('array')
+        expect(result.data.viewer.products[0].id).to.not.be.empty()
+        expect(result.data.viewer.products[0].sku).to.not.be.empty()
+        expect(result.data.viewer.products[0].name).to.not.be.empty()
+        expect(result.data.viewer.products[0].description).to.not.be.empty()
+        expect(result.data.viewer.products[0].curr).to.not.be.empty()
+        expect(result.data.viewer.products[0].price).to.not.be.empty()
+      })
+    })
+  })
+    
+})
